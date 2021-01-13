6d8146dfe5930730becf9e713b9b1abc0e42764f
baozhoutao <baozhoutao@hotoa.com>
2021-01-13 18:21:02 +0800

    简档添加属性：is_external , 如果为true 则表示为外部用户。供应商、客户is_external 为 true。user、admin 为false. #1072

--- packages/standard-objects/permission_set.client.js
+++ packages/standard-objects/permission_set.client.js
@@ -16,7 +16,17 @@ Steedos.PermissionSetManager.changeSchema = function(doc, schema){
         schema._schema.password_history = objectSchema.password_history;
         schema._schema.max_login_attempts = objectSchema.max_login_attempts;
         schema._schema.lockout_interval = objectSchema.lockout_interval;
+        if(doc.name === 'supplier' || doc.name === 'customer'){
+            schema._schema.is_external.autoform.readonly = true;
+            schema._schema.is_external.autoform.disabled = true;
+            doc.is_external = true;
+        }else{
+            schema._schema.is_external.autoform.readonly = false;
+            schema._schema.is_external.autoform.disabled = false;
+            doc.is_external = doc.is_external;
+        }
     }else{
+        hiddenField('is_external');
         hiddenField('password_history');
         hiddenField('max_login_attempts');
         hiddenField('lockout_interval');
